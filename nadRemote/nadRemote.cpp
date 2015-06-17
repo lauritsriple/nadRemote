5174514d7dd71a3c115e7259595ec28065b88fbd
lauritsriple <laurits.triple@gmail.com>
2015-06-17 13:39:41 +0200

    First commit

---
+++ nadRemote/nadRemote.cpp
@@ -0,0 +1,18 @@
+/*
+ * nadRemote.cpp
+ *
+ * Created: 17/06/2015 13:12:17
+ *  Author: Lua
+ */ 
+
+#define 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
